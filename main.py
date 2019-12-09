ed0b8fc6ff51228857333c0e1b239e99b6f82019
wonderpops <supstump@gmail.com>
2019-12-09 23:57:26 +1000

    Add main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import sys
+from token import Token, Tokeniser
+
+path = sys.argv[1]
+f = open(path, 'r')
+lex = Tokeniser(''.join(f.readlines()))
+
+while True:
+    t = lex.Next()
+    if t.tokenType == Token.tokenTypeEOF:
+        break
+    print(t)
