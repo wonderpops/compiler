4e54679b1753613637a49ff42d06615f98784f66
AlbinaKufarova <albina.kufarova@mail.ru>
2019-12-23 17:30:50 +1000

    Add cyrillic symbols check

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 from tokeniser import Token, Tokeniser
 
 path = sys.argv[1]
-f = open(path, 'r')
+f = open(path, 'r', encoding = 'utf-8')
 lex = Tokeniser(''.join(f.readlines()))
 
 while True:
