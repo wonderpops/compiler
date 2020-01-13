cdf080af529d6c71956ef14360359bf4ace5e5ce
wonderpops <supstump@gmail.com>
2020-01-13 18:29:33 +1000

    Add tree printer in main.py

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 import sys
 from tokeniser import Token, Tokeniser
 from parser1 import Parser
+from nodes import *
 
 path = sys.argv[2]
 f = open(path, 'r', encoding = 'utf-8')
 lex = Tokeniser(''.join(f.readlines()))
+
+def parseTree(deep, node):
+    deep += 1
+    if type(node) == UnaryOpNode:
+        return '  '*deep + '└ ' + node.op,  parseTree(deep, node.left)
+    elif type(node) == BinaryOpNode:
+        return '  '*deep + '└ ' + node.op.value, parseTree(deep, node.left), parseTree(deep, node.right)
+    else:
+        return ['  '*deep + '└ ' + str(node.value)]
+
+def printTree(node):
+    if len(node)>1 and type(node) == tuple or type(node) == list:
+        for f in node:
+            printTree(f)
+            if type(f) == str:
+                print(''.join(f))
+
 
 if sys.argv[1] == 'T':
     while True:
@@ -18,4 +36,5 @@ if sys.argv[1] == 'T':
             print(t)
 elif sys.argv[1] == 'P':
     p = Parser(lex)
-    print(p.ParseExpr())
+    x = parseTree(0, p.ParseExpr())
+    printTree(x)
